ea3df2625efb966b6857866936b73abf4d1dd4b0
RLucas1102 <robenoltl@gmail.com>
2025-12-05 17:45:40 -0500

    Changed ben day shader to use lighting calculation with dots instead of simple intensity values

--- shaders/BenDayShader/shader.fs
+++ shaders/BenDayShader/shader.fs
@@ -1,4 +1,20 @@
 #version 330 core
+layout (std140) uniform pLight {
+    vec4 plightAmbient;
+    vec4 plightDiffuse;
+    vec4 plightSpecular;
+    vec4 plightPos;
+    float constant;
+    float linear;
+    float quadratic;
+};
+
+layout (std140) uniform DirLight {
+    vec4 dirLightDir;
+    vec4 dirLightAmb;
+    vec4 dirLightDif;
+    vec4 dirLightSpc;
+};
 
 layout (std140) uniform Material {
     vec4 diffuseIn;
@@ -15,22 +31,18 @@ in vec3 fragPos;
 in mat4 viewMat;
 
 vec3 circle(vec3 _fragPos, float _radius);
-float mySmoothStep(float edge0, float edge1, float value);
 vec3 tile(vec3 _fragPos, float _zoom);
+vec3 CalcPLight();
+vec3 CalcDirLight();
+
+float toonScale = 1.0f / toonLevels;
 
 void main() {
 
-    float intensity;
-    vec3 color;
-    vec3 norm = normalize(normal);
-
-    intensity = dot(vec3(0.0f, -1.0f, 1.0f), norm);
-
-    vec3 st = fragPos;
-    st = tile(st, dotTiling);
-
-    color = vec3(1.0f - circle(st, min(max(1.0 - intensity, 0.1), 0.9)));
-    fragColor = vec4(color, 1.0f);
+    vec3 result = vec3(0.0f, 0.0f, 0.0f);
+    //result += CalcDirLight();
+    result += CalcPLight();
+    fragColor = vec4(result, 1.0f);
 
 }
 
@@ -47,8 +59,56 @@ vec3 tile(vec3 _fragPos, float _zoom) {
     return fract(_fragPos);
 }
 
-float mySmoothStep(float edge0, float edge1, float value) {
-    float t;
-    t = clamp((value - edge0) / (edge1 - edge0), 0.0, 1.0);
-    return t * t * (3.0 - 2.0 * t);
+vec3 CalcDirLight() {
+
+    vec3 st = fragPos;
+    st = tile(st, dotTiling);
+
+    // Create a light factor for global illumination
+    vec3 ambient = vec3(dirLightAmb);
+
+    // Create a light factor for diffuse light
+    // First normalize all relevant vectors and find direction from frag to light
+    vec3 norm = normalize(normal);
+    vec3 lightDir = normalize(-vec3(viewMat * vec4(vec3(dirLightDir), 1.0f)));
+
+    // Calculate diffuse factor
+    float diff = max(dot(norm, lightDir), 0.0); // If angle is negative, make 0.0
+    diff = ceil(diff * toonLevels) * toonScale;
+
+    vec3 diffuse = vec3(dirLightDif) * (vec3(1.0f - circle(st, min(max(1.0 - diff, 0.1), 0.9))));
+
+    // Multiply lighting factors by object color and output
+    vec3 result = ambient + diffuse;
+
+    return result;
+}
+
+vec3 CalcPLight() {
+
+    vec3 st = fragPos;
+    st = tile(st, dotTiling);
+
+    // Need light position in view space
+    vec3 viewLightPos = vec3(viewMat * vec4(vec3(plightPos), 1.0f)); // Passing in vec4 caused an error when multiplying view matrix
+
+    // Create a light factor for global illumination
+    vec3 ambient = vec3(plightAmbient);
+
+    // Create a light factor for diffuse light
+    // First normalize all relevant vectors and find direction from frag to light
+    vec3 norm = normalize(normal);
+    vec3 lightDir = normalize(viewLightPos - fragPos);
+
+    // Calculate diffuse factor
+    float diff = max(dot(norm, lightDir), 0.0); // If angle is negative, make 0.0
+    diff = ceil(diff * toonLevels) * toonScale;
+
+    vec3 diffuse = vec3(plightDiffuse) * (vec3(1.0f - circle(st, min(max(1.0 - diff, 0.1), 0.9))));
+
+    // Multiply lighting factors by object color and output
+    vec3 result = ambient + diffuse;
+
+    return result;
+
 }
